a367609f73a1addfe886f00c1a10a33ab63997b2
marinagreen <60858629+marinagreen@users.noreply.github.com>
2021-08-10 01:05:45 +0000

    Started adding resources about what the quiz will be on.

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+
+</script>
+
+<h1>Russian Language Quiz</h1>
+
+
+<!-- Recources Page -->
+<h2>Level 1 - Russian Numbers</h2>
+<p> One - "A-deen" - один </p>
+<p> Two - "Dva" - два </p>
+<p> Three - "Tri" - три </p>
+<p> Four - "Chi-ty-re" - Четыре </p>
+<p> Five - "Pya-st" - пять </p>
+<p> Six - "Shae-st" - шесть </p>
+<p> Seven - "Sem" - Семь </p>
+<p> Eight - "Vo-Sem" - восемь</p>
+<p> Nine - "Dev-yat" - девять </p>
+<p> Ten - "Des-yat" - десять</p>
+
+<h2>Level 2 - Russian Greetings</h2>
+<p>Hello (Informal) - "Dratz-vuy-chor" - </p>
+<p>Hello (Formal) - "Pri-vi-et" - </p>
+<p>How are you? (Informal) - "Kak De-la" - </p>
+<p>How are you? (Formal) - "Kak Vashi De-la" - </p>
+<p>I'm good - "Ya Kha-ra-shor" - </p>
+<p>I'm tired - "Ya Uti-Blak"</p>
+<p>I'm hungry - "Ya </p>
+<p>What is your name? - </p>
+<p>My name is (Name) - "Men ya za-vout (Name)" - </p>
+
+<h2>Level 3 - Extra Greetings</h2>
+<p>Good Morning - "Do-bre U-tra"</p>
+<p>Good Day - "Do-bre De-in"</p>
+<p>Good Afternoon - "Do-bre Vet-cher"</p>
