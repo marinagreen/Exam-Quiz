fce6f7c721d7bedc8c6548f65d796191e431b8bc
marinagreen <60858629+marinagreen@users.noreply.github.com>
2021-08-13 00:16:52 +0000

    starting to set up the layout, moved the resources to a resource page.

--- src/App.svelte
+++ src/App.svelte
@@ -1,54 +1,25 @@
 <script>
+import resources from './Resource.svelte';
+
+let first = ''
 
 </script>
 
-<h1>Russian Language Quiz</h1>
+<!-- Choose Level, e.g. 1,2,3, > resource page for the level shows, once ready begin -->
+<!--Btn start, gets rid of resouce page, shows first question, once answered, removes question and shows the next question, repeats until ten questions.-->
 
+<!-- IDEA - add a hint button?? -->
 
-<!-- Recources Page -->
+<!-- 1ST QUESTION - "How do you say..." -->
 
+<!-- Level 1 : Question 1
+How do you say one in Russian?
+a. "a-deen", b."voy-shem", c. "dva" -->
 
-<h2>Level 1 - Russian Numbers</h2>
+<!-- Level 2 : Question 1
+How do you say red?
+a. "gulaboi", b."zillo-nyi", c. "krast-nyi" -->
 
-<p> One - "A-deen" - один </p>
-<p> Two - "Dva" - два </p>
-<p> Three - "Tri" - три </p>
-<p> Four - "Chi-ty-re" - Четыре </p>
-<p> Five - "Pya-st" - пять </p>
-<p> Six - "Shae-st" - шесть </p>
-<p> Seven - "Sem" - Семь </p>
-<p> Eight - "Vo-Sem" - восемь</p>
-<p> Nine - "Dev-yat" - девять </p>
-<p> Ten - "Des-yat" - десять</p>
-
-<p></p>
-
-<h2>Level 2 - Russian Colours</h2>
-
-<p> Red - "krast-nyi" - красный </p>
-<p> Green - "ze-lyo-nyi" - зелёный </p>
-<p> Yellow - "zhyol-tyi" - жёлтый</p>
-<p> Light Blue - "goluboi" - голубой</p>
-<p> Dark Blue - "sinii" - синий </p>
-<p> Black - "chyor-nyi" - чёрный</p>
-<p> White - "bel-yi" - белый</p>
-<p> Orange - "oranzhevyi" - оранжевый</p>
-<p> Pink - "rozo-vyi" - розовый </p>
-<p> Purple - "fioleto-vyi" - фиолетовый </p>
-
-<p></p>
-
-<h2>Level 3 - Russian Greetings</h2>
-
-
-<p>Hello (Informal) - "Pri-vi-et" - Привет</p>
-<p>Hello (Formal) - "Dratz-vuy-chor" - здороваться</p>
-<p>How are you? (Informal) - "Kak De-la" - как дела?</p>
-<p>How are you? (Formal) - "Kak Vashi De-la" - как ваши дела</p>
-<p>I'm good - "Ya Kha-ra-shor" - я хороший</p>
-<p>I'm tired - "Ya U-stal" - я устал</p> 
-<p>What is your name? - как вас зовут?</p>
-<p>My name is (Name) - "Men ya za-vout (Name)" - меня зовут (имя)</p>
-<p>Goodbye (Informal) - "Poka-Poka" - пока пока</p>
-<p>Goodbye (Formal) - "Das-va-dan-ya" - 
-до свидания</p>
+<!-- Level 3 : Question 1 
+How do you say hello (formal)?
+a. "privet", b."dratzvutyre", c. "kak dela" -->
